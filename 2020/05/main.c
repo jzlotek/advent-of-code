0f07b83fe1968f48968445f03914cbb115f1a06b
John Zlotek <jzlotek@gmail.com>
2020-12-07 18:46:39 -0500

    Mon Dec  7 06:46:39 PM EST 2020

--- 2020/05/main.c
+++ 2020/05/main.c
@@ -4,22 +4,24 @@ int getid(INT_ARRAY line) {
   int id = 0;
 
   for (unsigned int i = 0; i < line->currsize; i++) {
-    if (arr_get(line, i) == 'L' || arr_get(line, i) == 'F') {
-      id++;
-    }
+    id += (arr_get(line, i) == 'B' || arr_get(line, i) == 'R');
     id <<= 1;
   }
+  id >>= 1;
 
   return id;
 }
 int main() {
   INT_MATRIX mat = parse_mat_chars();
   INT_ARRAY nums = new_int_array(0);
+
   for (unsigned int i = 0; i < mat->currheight; i++) {
     append(nums, getid(mat->data[i]));
   }
+
   arrsort(nums);
   printf("%d\n", arr_get(nums, -1));
+
   for (unsigned int i = 1; i < nums->currsize - 1; i++) {
     if (arr_get(nums, i) - arr_get(nums, i - 1) == 2) {
       printf("%d\n", arr_get(nums, i) - 1);
